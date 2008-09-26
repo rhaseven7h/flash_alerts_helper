eb2415bd0bdcb2318bdd38f8e7b35cf009a39b3f
Gabriel Medina <rha7.com@gmail.com>
2008-09-26 17:59:07 -0600

    NEW: Created.

---
+++ lib/flash_alerts.rb
@@ -0,0 +1,17 @@
+# FlashAlerts
+module FlashAlertsHelper
+	def flash_alerts
+		display=false
+		str = String.new
+		flash.each do |key, value|
+			if value && value.strip.length > 0
+				str += %Q(<div class='flash_message' id='flash_#{key.to_s}'>#{value}</div>)
+				display = true
+			end
+		end
+		str = %Q(<div id='flash_message_box'>#{str}</div>)
+		return display ? str : String.new
+	end
+end
+
+
---
+++ tasks/flash_alerts_tasks.rake
@@ -0,0 +1,4 @@
+# desc "Explaining what the task does"
+# task :flash_alerts do
+#   # Task goes here
+# end
